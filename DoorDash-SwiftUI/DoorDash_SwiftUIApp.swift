b694acc6970310b0803a8691caebdae7192afa1b
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-02-23 14:03:33 -0800

    Initial Commit

---
+++ DoorDash-SwiftUI/DoorDash_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorDash_SwiftUIApp.swift
+//  DoorDash-SwiftUI
+//
+//  Created by ReetDhillon on 2024-02-23.
+//
+
+import SwiftUI
+
+@main
+struct DoorDash_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
